0ea8c1d90b9960a5d935aabb23217fce1ef29cd2
heram <heramba_2006@yahoo.com>
2021-08-03 13:27:57 +0530

    added actual gpx file

--- src/main/java/com/example/kafka/producer/Producer.java
+++ src/main/java/com/example/kafka/producer/Producer.java
@@ -42,7 +42,7 @@ public class Producer {
             stream.forEach(line -> {
             logger.info("message******* " + line);
             try {
-				Thread.sleep(500);
+				Thread.sleep(10);
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
